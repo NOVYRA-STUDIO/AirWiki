7fc5ae4c619d9dab2d162fdce86f2ac441bc96e7
NOVYRA-STUDIO <missbordug09@gmail.com>
2026-01-04 20:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function loadContent(section) {
+function loadContent(section, event) {
   const content = document.getElementById("content");
+
+  // Активний пункт меню
+  document.querySelectorAll('.sidebar li').forEach(li => li.classList.remove('active'));
+  event.target.classList.add('active');
 
   const data = {
     about: `
