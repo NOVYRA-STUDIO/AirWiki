12f316e78cc4c30b679b40b1fe7c519c31f1b306
NOVYRA STUDIO™ <missbordug09@gmail.com>
2026-01-05 02:30:25 +0200

    Add margin to Air1 header in script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function loadContent(section, event) {
         <li>Радіус дії: 256px</li>
       </ul>
 
-      <h2>Air1</h2>
+      <h2 style="margin-top:20px;">Air1</h2>
       <ul>
         <li>Швидкість: 300px/c.</li>
         <li>Швидкість повороту: 15px/с.</li>
